1c50cf600616e3605d03812e372927d51f4aa25d
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-17 17:53:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution in this file!
+driver= {}
+updateDriverWithKeyAndValue(driver,key,value){
+  return Object.assign({},driver,{[key]:value})
+}
+destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+  return Object.assign(driver,{[key]:value})
+}
